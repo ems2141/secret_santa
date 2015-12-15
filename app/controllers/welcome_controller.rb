bc879bfb25133aee3655d57712c51573ad012d1a
Ellie Schneiders <pair+ellie@pivotal.io>
2015-12-14 20:32:04 -0700

    Display generator results on index page when a comma separated list of people is submitted

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,12 @@
 class WelcomeController < ApplicationController
   def index
 
+    if request.xhr?
+      people_array = params[:people].split(',')
+
+      assigned_list = SecretSantaGenerator.new(people_array).create_list
+
+      render plain: assigned_list
+    end
   end
 end
