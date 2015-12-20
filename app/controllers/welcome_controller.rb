4eb141ada0535ec90945e94331e3a27426442ba0
Ellie Schneiders <pair+ellie@pivotal.io>
2015-12-20 07:57:42 -0700

    Format pairs list

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -7,6 +7,7 @@ class WelcomeController < ApplicationController
       # assigned_list = SecretSantaGenerator.new(people_array).create_list
       assigned_list = SecretSantaGenerator.new(people_array).create_random_list
 
+      assigned_list = view_context.render("santa_list", {pairs: assigned_list})
       render plain: assigned_list
     end
   end
